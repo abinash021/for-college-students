d550fd431d8ea9f7b8221ffbabcafcf0e5cbdcf7
BASUDEV ROUT <57030150+basu021@users.noreply.github.com>
2021-12-30 16:10:13 +0530

    ,

---
+++ Data structure using C/exp-3/01_.java
@@ -0,0 +1,12 @@
+class print_1_to_10
+piblic static void main(String arg[])
+{
+    int i=1;
+    System.out.println("Output is:");
+    while(i<=10)
+    {
+        System.out.println(i);
+        i++;
+    }
+}
+}
